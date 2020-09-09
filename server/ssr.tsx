9b87f8c4db4eff97178261cd99a439c11eb1ac92
Tom McLaughlin <tom@codedown.io>
2020-09-09 04:02:16 -0600

    Simplify ssr.tsx

--- server/ssr.tsx
+++ server/ssr.tsx
@@ -16,7 +16,6 @@ import {fetch} from "cross-fetch";
 
 import App from "../app/App";
 import {inMemoryCacheArgs} from "../app/cacheTypePolicies";
-import {getHTMLFragments} from "./client";
 
 const isomorphicCookie = require("isomorphic-cookie");
 
@@ -58,8 +57,6 @@ export function end(endingHTMLFragment) {
   return end;
 }
 
-const stream = false;
-
 export default async function ssr(req, res) {
   try {
     console.log("Handling request", req.originalUrl);
@@ -78,54 +75,26 @@ export default async function ssr(req, res) {
 
     await getDataFromTree(app);
 
-    if (stream) {
-      // TODO: go back to streaming once we figure out how to set status
-      const [startingHTMLFragment, endingHTMLFragment] = getHTMLFragments({
-        drainHydrateMarks: printDrainHydrateMarks()
-      });
+    await whenComponentsReady();
 
-      res.status(200);
-      res.write(startingHTMLFragment);
-      const sheet = new ServerStyleSheet();
-      const stream =  sheet.interleaveWithNodeStream(
-        renderToNodeStream(sheet.collectStyles(app))
-      );
-      stream.pipe(through(write, end(endingHTMLFragment))).pipe(res);
-    } else {
-      await whenComponentsReady();
+    const html = renderToString(sheets.collect(app));
 
-      const html = renderToString(sheets.collect(app));
+    res.status(200);
 
-      if (context.url) {
-        // Note: use 302 here, because browsers often cache 301s. This causes problems where
-        // logging out and back in as another user and then visiting /profile for a redirect
-        // can cause you to get the original user's page due to the browser caching the redirect.
-        res.redirect(context.status || 302, context.url);
-      } else {
-        if (context.status) {
-          res.status(context.status);
-        } else {
-          res.status(200);
-        }
+    // Grab the CSS from the sheets.
+    const css = sheets.toString();
 
-        // Grab the CSS from the sheets.
-        const css = sheets.toString();
+    // Get the initial GraphQL state
+    const initialState = client.extract();
 
-        // Get the initial GraphQL state
-        const initialState = client.extract();
+    const htmlPath = path.join(process.cwd(), "dist", "client", "index.html");
+    let fullHtml = fs.readFileSync(htmlPath).toString();
+    fullHtml = fullHtml.replace("<!--META-->", `<script>window.__APOLLO_STATE__ = ${JSON.stringify(initialState)};</script>\n<style id="jss-server-side">${css}</style>\n`)
+    fullHtml = fullHtml.replace("<!--MARKS-->", printDrainHydrateMarks() + "\n");
+    fullHtml = fullHtml.replace(`<div id="app">`, `\n<div id="app">${html}</div>\n`)
 
-        const htmlPath = path.join(process.cwd(), "dist", "client", "index.html");
-        let fullHtml = fs.readFileSync(htmlPath).toString();
-        fullHtml = fullHtml.replace("<!--META-->", `<script>window.__APOLLO_STATE__ = ${JSON.stringify(initialState)};</script>\n<style id="jss-server-side">${css}</style>\n`)
-        fullHtml = fullHtml.replace("<!--MARKS-->", printDrainHydrateMarks() + "\n");
-        fullHtml = fullHtml.replace(`<div id="app">`, `\n<div id="app">${html}</div>\n`)
-
-        res.write(fullHtml);
-        res.end();
-      }
-    }
-
-
+    res.write(fullHtml);
+    res.end();
   } catch (e) {
     log.error(e);
     res.status(500);
