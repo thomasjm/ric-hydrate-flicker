1fcc1cce8b913c50ceaae05ef5ed34d86eec1ece
Tom McLaughlin <tom@codedown.io>
2020-09-11 18:47:38 -0600

    Try using importedComponent

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,11 @@
 
 import * as React from "react";
 import {Switch, Route, useLocation} from "react-router-dom";
-import {lazy, LazyBoundary, useImported} from "react-imported-component";
+import importedComponent from "react-imported-component";
 
 import "regenerator-runtime/runtime";
 
-function importPage<P>(importFn: () => React.FunctionComponent<P>, props?: P) {
-  const { imported, loading } = useImported(importFn);
-
-  return loading ? <div /> : <imported {...props} />;
-}
-
-const Simple = importPage(() => import("./pages/simple"));
+const Simple = importedComponent(() => import("./pages/simple"));
 
 export default function App() {
   return (
